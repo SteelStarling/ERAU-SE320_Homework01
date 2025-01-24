47f38cedcfb6619b269e890fb6c306c2c25b3d3f
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-01-24 13:45:41 -0700

    Fixed infinte_generators.py to be better commented and formatted

--- infinite_generators.py
+++ infinite_generators.py
@@ -21,14 +21,15 @@ def fibonacci(n: int = None) -> Generator[int, None, None]:
     Optional Arguments:
         n (int): The number of values to output
     """
+
     last_two = [0, 1] # define to first two numbers, will be replaced each time
 
     # for loop (since we need to know how many loops)
     for i in count(0):
-        # break out of loop once n is reached (if it exists)
+        # once n is reached (if it exists), stop generating
         if n is not None and i >= n:
             break
-        
+
         # first two outputs hardcoded
         if i == 0:
             yield last_two[0]
@@ -55,12 +56,13 @@ def recaman(n: int) -> Generator[int, None, None]:
     Optional Arguments:
         n (int): The number of values to output
     """
+
     a_prev = 0
     recaman_set = set()
 
     # for loop (since we need to know how many loops)
     for i in count(0):
-        # break out of loop once n is reached (if n describes an end point)
+        # once n is reached (if it exists), stop generating
         if n is not None and i >= n:
             break
 
@@ -75,8 +77,8 @@ def recaman(n: int) -> Generator[int, None, None]:
         # if a_n <= 0 or in set, a_n = a_prev + (i + 1)
         if a_n <= 0 or a_n in recaman_set:
             a_n = a_prev + (i + 1)
-        
-        # update a_prev
+
+        # update previous value
         a_prev = a_n
 
 
@@ -99,25 +101,25 @@ if __name__ == "__main__":
 
     # Testing Work
     # print("========== FIBBONACI ==========")
-    # 
+    #
     # print("Infinite Fibbonaci:")
     # infinite_fibbonaci = fibbonaci()
     # for i in range(50):
     #     print(f"{i + 1}.  {next(infinite_fibbonaci)}")
-    # 
+    #
     # print("\nFinite Fibbonaci:")
     # short_fibbonaci = fibbonaci(5)
     # for i, fib in enumerate(short_fibbonaci):
     #     print(f"{i + 1}.  {fib}")
-    # 
+    #
     # print("\n\n")
     # print("=========== RECAMAN ===========")
-    # 
+    #
     # print("Infinite Recaman:")
     # infinite_recaman = recaman()
     # for i in range(50):
     #     print(f"{i + 1}.  {next(infinite_recaman)}")
-    # 
+    #
     # print("\nFinite Recaman:")
     # short_recaman = recaman(5)
     # for i, rec in enumerate(short_recaman):
